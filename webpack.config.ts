9c57f031aace674d38cfe5f678bb6ad0caa86db7
unknown <aronilie.code@gmail.com>
2024-08-14 22:50:21 +0300

    Added webpack loaders

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,13 @@
-const deps = require("./package.json").dependencies;
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const deps = require('./package.json').dependencies;
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (_: any, argv: any) => ({
   mode: (process.env.NODE_ENV as 'production' | 'development' | undefined) ?? 'development',
 
   output: {
-    publicPath:
-      argv.mode === "development"
-        ? "http://localhost:5100/"
-        : "https://smart-form-rqmr.onrender.com/",
+    publicPath: argv.mode === 'development' ? 'http://localhost:5100/' : 'https://smart-form-rqmr.onrender.com/',
   },
 
   resolve: {
@@ -25,24 +22,37 @@ module.exports = (_: any, argv: any) => ({
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        use: 'babel-loader',
+        test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+      {
+        test: /\.png$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
       },
     ],
   },
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "smartForm",
-      filename: "remoteEntry.js",
+      name: 'smartForm',
+      filename: 'remoteEntry.js',
       remotes: {},
       exposes: {
-        "./SmartForm": "./src/SmartForm.tsx",
+        './SmartForm': './src/SmartForm.tsx',
       },
       shared: {
         ...deps,
@@ -50,14 +60,14 @@ module.exports = (_: any, argv: any) => ({
           singleton: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     }),
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new Dotenv({ systemvars: true }),
   ],
