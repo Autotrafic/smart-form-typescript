e29c26dd4e58236e59245718312c7f3b70a9de32
unknown <aronilie.code@gmail.com>
2024-09-12 00:44:06 +0300

    Prod

--- src/modules/core/utils/urls.ts
+++ src/modules/core/utils/urls.ts
@@ -1,6 +1,6 @@
 export const TERMS_URL = 'https://autotrafic.es/terminos-generales/';
 export const PRIVACY_URL = 'https://autotrafic.es/politica-de-privacidad/';
-export const UPLOAD_DOCUMENTS_URL = 'http://localhost:5300';
+export const UPLOAD_DOCUMENTS_URL = 'https://upload-customer-files.onrender.com';
 
 const prodApiUrl = 'https://api.autotrafic.es';
 const localApiUrl = 'http://localhost:3100';
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,6 @@ const deps = require('./package.json').dependencies;
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (_: any, argv: any) => ({
   mode: (process.env.NODE_ENV as 'production' | 'development' | undefined) ?? 'development',
