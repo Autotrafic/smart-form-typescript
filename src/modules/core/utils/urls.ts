c42e0470d5cdb238c4f52e6e78d559ee5b76e131
unknown <aronilie.code@gmail.com>
2024-09-11 23:57:24 +0300

    Production

--- src/modules/core/utils/urls.ts
+++ src/modules/core/utils/urls.ts
@@ -5,4 +5,4 @@ export const UPLOAD_DOCUMENTS_URL = 'http://localhost:5300';
 const prodApiUrl = 'https://api.autotrafic.es';
 const localApiUrl = 'http://localhost:3100';
 
-export const BASE_API_URL = localApiUrl;
+export const BASE_API_URL = prodApiUrl;
