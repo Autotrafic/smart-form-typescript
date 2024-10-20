4deba0d48f516b2902b2c9823cef9fffdd04627e
unknown <aronilie.code@gmail.com>
2024-10-20 18:37:07 +0300

    Updated documents url

--- src/modules/core/utils/urls.ts
+++ src/modules/core/utils/urls.ts
@@ -1,5 +1,5 @@
 export const TERMS_URL = 'https://autotrafic.es/terminos-generales/';
 export const PRIVACY_URL = 'https://autotrafic.es/politica-de-privacidad/';
-export const UPLOAD_DOCUMENTS_URL = 'https://upload-customer-files.onrender.com';
+export const UPLOAD_DOCUMENTS_URL = 'https://documentos.autotrafic.es';
 
 export const WHATSAPP_API_URL = 'https://autotrafic-whatsapp-d396136eabe5.herokuapp.com';
