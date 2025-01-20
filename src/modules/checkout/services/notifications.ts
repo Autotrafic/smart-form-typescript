ea7e76d351260810824cb4e3d45dff3650ad9f4f
unknown <aronilie.code@gmail.com>
2025-01-20 09:21:06 +0100

    Added check

--- src/modules/checkout/services/notifications.ts
+++ src/modules/checkout/services/notifications.ts
@@ -3,11 +3,18 @@ import { createWhatsAppConfirmationMessage } from '../utils/funcs';
 import { autotraficApi } from '@modules/core/services';
 
 export async function sendWhatsAppConfirmation(orderData: IOrder) {
-  const message = createWhatsAppConfirmationMessage(orderData);
+  try {
+    const message = createWhatsAppConfirmationMessage(orderData);
 
   const phoneNumber = orderData.vehicleForm.phoneNumber.replace(/\D/g, '');
 
   await autotraficApi.notification.sendWhatsapp({ phoneNumber, message });
+  } catch (error) {
+    const receivedError = error as any;
+
+    await sendSlackNotification(`Error enviando la notificación de pedido realizado. No se frena el trámite. ${receivedError.response}`);
+  }
+  
 }
 
 export async function sendSlackNotification(message: string, channel?: 'orders') {
